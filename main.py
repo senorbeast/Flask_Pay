65f41b0130e3acb14b22fb1c2bf42e5609aa5f1f
senorbeast <hrishikesh99@hotmail.com>
2022-02-13 23:38:24 +0530

    Quering before commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import json
 import sqlite3
+import datetime
 
 ## Transfering money across two bank Accounts.
 
@@ -24,8 +25,9 @@ import sqlite3
 # [ ] A,B transfer money to C at same time :
 # ! SERIALIZATION locking and unlocking db for read/write for 1 operation and async await flask ?
 # ! exclusive lock to remove read access...
-# Flask has inbuilt concurrency through its WSGI/ASGI production server.
-
+# Flask has inbuilt concurrency through its WSGI/ASGI production server. but need to wait for locked database
+# https://www.sqlite.org/lockingv3.html
+#
 
 app = Flask(__name__)
 app.config["JSON_SORT_KEYS"] = False
@@ -56,7 +58,39 @@ def ldict_bal(cursor):
 # * Transaction Table : Adding new transactions
 @app.route("/transfer", methods=["POST"])
 def transfer():
+    """
+    Transfer API, Method = POST,
 
+    request body: {
+        "from": "account_no",
+        "to": "account_no",
+        "amount": "money"
+        }
+    Balances Table
+        Debit amount from "From A/C"
+        Credit amount to "TO A/C"
+
+    Transacton Table
+        "From A/C" -amount date_created
+        "To A/C"   +amount date_created
+
+    commit changes to db only if no errors occur and db is online else rollback changes
+
+    :return: json object
+            {
+            "id": "transaction_id",
+            "from":{
+                "id": "account_no",
+                "balance": "current_balance"
+            },
+            "to":{
+                "id": "account_no",
+                "balance": "current_balance"
+            },
+            "transfered": "transfer_amount"
+            "created_datetime": "transaction created time"
+            }
+    """
     from_account_no = request.form["from_account_no"]
     to_account_no = request.form["to_account_no"]
     amount = request.form["amount"]
@@ -67,8 +101,8 @@ def transfer():
         # if DB is available after request and before commit
         try:
             #! some how all these in 1 string dont work ?!!
-            lock = """ PRAGMA locking_mode = EXCLUSIVE;
-            BEGIN EXCLUSIVE;"""
+            # lock = """ PRAGMA locking_mode = EXCLUSIVE;
+            # BEGIN EXCLUSIVE;"""
             debit = """UPDATE balancet 
                 SET balance = balance - {amt} 
                 WHERE account_no = {fa}
@@ -96,61 +130,63 @@ def transfer():
             """.format(
                 amt=amount, ta=to_account_no
             )
-
             # We are not using these cursor objects, we would name them same.
 
             cursord = conn.execute(debit)
             cursorc = conn.execute(credit)
             cursor_from_txa = conn.execute(update_from_txa)
             cursor_to_txa = conn.execute(update_to_txa)
+            txa_rows = conn.execute(
+                "SELECT * FROM transactionst ORDER BY id DESC LIMIT 2;"
+            )
+
+            balance_row_from = conn.execute(
+                "SELECT * FROM balancet WHERE account_no={fa} ".format(
+                    fa=from_account_no
+                )
+            )
+            balance_row_to = conn.execute(
+                "SELECT * FROM balancet WHERE account_no={ta} ".format(ta=to_account_no)
+            )
         except Exception as e:
             ## if DB fails before commit (no rollback reqd)
             if str(e) == "CHECK constraint failed: balance >=0":
                 return jsonify(
-                    {"Error": "Insufficient funds", "Status": "Transaction Failed!"}
+                    {"Error1": "Insufficient funds", "Status": "Transaction Failed!"}
                 )
             else:
-                return jsonify({"Error": str(e), "Status": "Transaction Failed!"})
+                return jsonify({"Error2": str(e), "Status": "Transaction Failed!"})
         # COMMITED TO DB
         conn.commit()
         try:
             # Need to commit to read from database,
             # could return payload w/o reading too ?
-            # Will give to_txa, from_txa (since desc order)
-            txa_rows = conn.execute(
-                "SELECT * FROM transactionst ORDER BY id DESC LIMIT 2;"
-            )
-
-            balance_rows = conn.execute(
-                "SELECT * FROM balancet WHERE account_no={fa} OR account_no={ta}".format(
-                    fa=from_account_no, ta=to_account_no
-                )
-            )
 
             txas = ldict_txa(txa_rows)[::-1]  # from_txa, to_txa
-            bals = ldict_bal(balance_rows)  # from_bal, to_bal
-            conn.close()
-
+            bals_from = ldict_bal(balance_row_from)  # from_bal, to_bal
+            bals_to = ldict_bal(balance_row_to)
+            print(bals_from, bals_to)
             payload = {
-                "id": txas[0]["id"],
+                "id": txas[0]["id"],  # From txa id
                 "from": {
-                    "id": bals[0]["account_no"],
-                    "balance": bals[0]["balance"],
+                    "id": bals_from[0]["account_no"],
+                    "balance": bals_from[0]["balance"],
                 },
                 "to": {
-                    "id": bals[1]["account_no"],
-                    "balance": bals[1]["balance"],
+                    "id": bals_to[0]["account_no"],
+                    "balance": bals_to[0]["balance"],
                 },
-                "transfered": txas[1]["amount"],  # to_txa
+                "transfered": txas[1]["amount"],  # to_txa amount
                 "created_datetime": txas[1]["created_datetime"],
             }
+            conn.close()
             return jsonify(payload)
         except Exception as e:
             # if DB fails after commit
             conn.rollback()
-            return jsonify({"Error": str(e), "Status": "Transaction Failed"})
+            return jsonify({"Error3": str(e), "Status": "Transaction Failed"})
     else:
-        return jsonify({"Error": "Server is down", "Status": "Transaction Failed!"})
+        return jsonify({"Error4": "Server is down", "Status": "Transaction Failed!"})
 
 
 # Transaction Table : Display
@@ -180,11 +216,11 @@ def balance():
     if request.method == "POST":
         conn = db_connection()
         cursor = conn.cursor()
-        account_no = request.form["account_no"]
+        # account_no = request.form["account_no"]
         balance = request.form["balance"]
-        sql = """INSERT INTO balancet
-            VALUES( {amt},{bal} )""".format(
-            amt=account_no, bal=balance
+        sql = """INSERT INTO balancet (balance)
+            VALUES( {bal} )""".format(
+            bal=balance
         )
         cursor.execute(sql)
         conn.commit()
